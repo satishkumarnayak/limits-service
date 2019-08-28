f5546a660d2abbd1070ee74da22a63ef05c8cec7
Satish Nayak <satish.nayak@changehealthcare.com>
2019-08-28 17:09:59 +0530

    Updated

--- src/main/java/com/instanceofcake/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ src/main/java/com/instanceofcake/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -8,21 +8,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.instanceofcake.microservices.limitsservice.bean.LimitConfiguration;
 import com.instanceofcake.microservices.limitsservice.config.Configuration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
-	
+
 	/*
 	 * @Autowired Environment environment;
 	 */
-	
+
 	@Autowired
 	Configuration config;
-	
+
 	@GetMapping("/limits")
 	public LimitConfiguration retriveLimitsFromConfigurations() {
 		return new LimitConfiguration(config.getMax(), config.getMin());
-	//	return new LimitConfiguration(Integer.parseInt(environment.getProperty("limits-service.max")), Integer.parseInt(environment.getProperty("limits-service.min")));
+		// return new
+		// LimitConfiguration(Integer.parseInt(environment.getProperty("limits-service.max")),
+		// Integer.parseInt(environment.getProperty("limits-service.min")));
 	}
 
+	@GetMapping("/limits-fault-tolerance")
+	@HystrixCommand(fallbackMethod="fallbackRetriveLimitsFromConfigurations")
+	public LimitConfiguration retriveLimitsFromConfigurationsFaultTolerance() {
+		throw new RuntimeException();
+		
+	}
+
+	public LimitConfiguration fallbackRetriveLimitsFromConfigurations() {
+		return new LimitConfiguration(999, 9);
+	}
 }
